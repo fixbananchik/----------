0ab2daa762a92b57a66ea4fd5a100ec287a40939
fixbananchik <128502663+fixbananchik@users.noreply.github.com>
2023-04-18 17:08:59 +0300

    chess

--- chess-main/chess/__init__.py
+++ chess-main/chess/__init__.py
@@ -1,3 +1,9 @@
-from bishop import bishop
+from .slon import slon 
+from .kon import kon 
+from .ladia import ladia 
+from .ferz import ferz
+from .korol import korol 
 
-bishop('c4')
+figura = input("Введите фигуру, которой хотите походить ")
+coordinates = input("Введиту координаты фигуры ")
+
