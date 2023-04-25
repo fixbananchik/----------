975cf5757f5b76baf1c48c76e93f684351ee6892
fixbananchik <128502663+fixbananchik@users.noreply.github.com>
2023-04-25 18:14:50 +0300

    Работать!!!

---
+++ Работать!!!/rabota.py
@@ -0,0 +1,8 @@
+class Rabotyaga:
+    def __init__(self, name, age, zarplata):
+        self.name = name
+        self.age = age
+        self.zarplata = zarplata
+    def rabotaaaa(self):
+        print(self.name, "работает за " self.zarplata, " в месяц")
+        
