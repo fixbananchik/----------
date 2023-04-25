330461f1d98742063328fccee0c2fcc381b26eef
fixbananchik <128502663+fixbananchik@users.noreply.github.com>
2023-04-25 18:32:47 +0300

    Работаааать

--- Работать!!!/rabota.py
+++ Работать!!!/rabota.py
@@ -4,5 +4,17 @@ class Rabotyaga:
         self.age = age
         self.zarplata = zarplata
     def rabotaaaa(self):
-        print(self.name, "работает за " self.zarplata, " в месяц")
-        
+        print(self.name, "работает за", self.zarplata, "в месяц")
+
+
+class Meneger(Rabotyaga):
+    def __init__(self, name, age, zarplata, kolvo_podchinnenih, koef):
+        super().__init__(name, age, zarplata)
+        self.kolvo_podchinnenih = kolvo_podchinnenih
+        self.koef = koef
+    def rabotaaaa(self):
+        print(self.name, "работает за", (self.zarplata + self.kolvo_podchinnenih * self.koef), "в месяц" )
+
+bob = Meneger("Боб", 30, 1000000, 40, 10000)
+
+bob.rabotaaaa()
