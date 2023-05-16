fdcb5aec2f7f75bb5c9b438e879984c28df021a2
fixbananchik <128502663+fixbananchik@users.noreply.github.com>
2023-05-16 14:15:26 +0300

    Список

---
+++ Список/spisok.py
@@ -0,0 +1,71 @@
+class noda:
+    def __init__(self, value, count):
+        self.value = value
+        self.link = None
+        self.count = count
+
+
+class str_list:
+    def __init__(self):
+        self.head = None
+
+    def push(self, storka):
+        new_noda = noda(storka)
+        if self.head == None:
+            self.head = new_noda
+        else:
+            current = self.head
+            while current.link != None:
+                current = current.link
+            current.link = new_noda
+
+    def pop(self):
+        if self.head == None:
+            pass
+        else:
+            current = self.head
+            prev = None
+            while current.link != None:
+                prev = current
+                current = current.link
+            prev.link = None
+
+    def print(self):
+        if self.head == None:
+            print('список пуст')
+        else:
+            index = 0
+            current = self.head
+            while current.link != None:
+                print(index, current.value)
+                current = current.link
+                index = index + 1
+            print(current.value, current.count)
+
+    def find(self, element_name):
+        if self.head == None:
+            return False
+        else:
+            current = self.head
+            while current.link != None:
+                if current.value == element_name:
+                    return True
+                current = current.link
+            if current.value == element_name:
+                return True
+            else:
+                return False
+                
+            
+
+
+
+
+hleb = noda("хлеб", 2)
+maslo = noda("масло", 1)
+smetana = noda("сметана", 3)
+luk = noda("лук", 4)
+
+Magazin = str_list()
+
+Magazin.print
